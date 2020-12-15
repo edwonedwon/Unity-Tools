7eef9451e0bf9baa97c544124338337b77766a31
Edwon <edwonia@gmail.com>
2020-12-15 16:40:01 +0900

    package version 1.1.1 - added Save scene keyboard shortcut

---
+++ Packages/Unity Tools/Editor/ForceSaveSceneAndProject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ForceSaveSceneAndProject : MonoBehaviour
+{
+    [MenuItem("File/Save project %&s")]
+    static void FunctionForceSaveProyect()
+    {
+        EditorApplication.ExecuteMenuItem("File/Save Project");
+        Debug.Log("Saved project");
+    }
+
+    [MenuItem("File/Save Scene And Project %#&s")]
+    static void FunctionForceSaveSceneAndProyect()
+    {
+        EditorApplication.ExecuteMenuItem("File/Save");
+        EditorApplication.ExecuteMenuItem("File/Save Project");
+        Debug.Log("Saved scene and project");
+    }
+}
