684069ca227434bf4fe093f4c4726cefba5d3a14
Edwon <edwonia@gmail.com>
2021-01-18 14:44:34 +0900

    refactored game event system back to one instead of separate classes for each paramter type

--- Packages/Unity Tools/Runtime/Game Event/GameEvent.cs
+++ Packages/Unity Tools/Runtime/Game Event/GameEvent.cs
@@ -8,23 +8,89 @@ namespace Edwon.Tools
     [CreateAssetMenu(fileName = "Game Event", menuName = "Scriptables/Game Event")]
     public class GameEvent : ScriptableObject
     {
-        private List<Action> listenerMethods = new List<Action>();
+        public enum ParameterType { None, String, GameObject }
+        public ParameterType parameterType;
+
+        private List<Action> listeners = new List<Action>();
+        private List<Action<string>> listenersString = new List<Action<string>>();
+        private List<Action<GameObject>> listenersGameObject = new List<Action<GameObject>>();
 
         public void Raise()
         {
-            for(int i = 0; i < listenerMethods.Count; i++)
-                listenerMethods[i]();
+            if (IsParameterTypeDifferent(ParameterType.None)) {return;};
+                
+            for(int i = 0; i < listeners.Count; i++)
+                listeners[i]();
+        }
+
+        public void Raise(string s)
+        {
+            if (IsParameterTypeDifferent(ParameterType.String)) {return;};
+
+            for(int i = 0; i < listenersString.Count; i++)
+                listenersString[i](s);
+        }
+
+        public void Raise(GameObject go)
+        {
+            if (IsParameterTypeDifferent(ParameterType.GameObject)) {return;};
+
+            for(int i = 0; i < listenersGameObject.Count; i++)
+                listenersGameObject[i](go);
         }
 
         public void RegisterListener(Action listener)
         { 
-            listenerMethods.Add(listener); 
+            if (IsParameterTypeDifferent(ParameterType.None)) {return;};
+            listeners.Add(listener); 
         }
 
         public void UnregisterListener(Action listener)
         { 
-            listenerMethods.Remove(listener); 
+            if (IsParameterTypeDifferent(ParameterType.None)) {return;};
+
+            listeners.Remove(listener); 
         }
 
+        public void RegisterListenerString(Action<string> listener)
+        { 
+            if (IsParameterTypeDifferent(ParameterType.String)) {return;};
+
+            listenersString.Add(listener); 
+        }
+
+        public void UnregisterListenerString(Action<string> listener)
+        { 
+            if (IsParameterTypeDifferent(ParameterType.String)) {return;};
+
+            listenersString.Remove(listener); 
+        }
+
+        public void RegisterListenerGameObject(Action<GameObject> listener)
+        { 
+            if (IsParameterTypeDifferent(ParameterType.GameObject)) {return;};
+
+            listenersGameObject.Add(listener); 
+        }
+
+        public void UnregisterListenerGameObject(Action<GameObject> listener)
+        { 
+            if (IsParameterTypeDifferent(ParameterType.GameObject)) {return;};
+
+            listenersGameObject.Remove(listener); 
+        }
+
+        public bool IsParameterTypeDifferent(ParameterType parameterType)
+        {
+            if (this.parameterType == parameterType)
+            {
+                return false;
+            }
+            else
+            {
+                Debug.Log(name + "'s parameter type is not " + parameterType);
+                return true;
+            }
+        }
     }
 }
--- Packages/Unity Tools/Runtime/Game Event/GameEventListener.cs
+++ Packages/Unity Tools/Runtime/Game Event/GameEventListener.cs
@@ -3,27 +3,60 @@ using UnityEngine.Events;
 using Edwon.Tools;
 
 namespace Edwon.Tools
-{   
+{
     public class GameEventListener : MonoBehaviour
     {
         public GameEvent gameEvent;
         public UnityEvent response;
-        
-        public void OnEventRaised()
-        {
-            response.Invoke();
-        }
+        public UnityEventString responseString;
+        public UnityEventGameObject responseGameObject;
 
         private void OnEnable()
-        {
-            if (gameEvent != null)
-                gameEvent.RegisterListener(OnEventRaised);
+        { 
+            switch(gameEvent.parameterType)
+            {
+                case GameEvent.ParameterType.None:
+                    gameEvent.RegisterListener(OnEventRaised); 
+                break;
+                case GameEvent.ParameterType.String:
+                    gameEvent.RegisterListenerString(OnEventRaised);
+                break;
+                case GameEvent.ParameterType.GameObject:
+                    gameEvent.RegisterListenerGameObject(OnEventRaised); 
+                break;
+            }
         }
 
         private void OnDisable()
         { 
-            if (gameEvent != null)
-                gameEvent.UnregisterListener(OnEventRaised); 
+            switch(gameEvent.parameterType)
+            {
+                case GameEvent.ParameterType.None:
+                    gameEvent.UnregisterListener(OnEventRaised); 
+                break;
+                case GameEvent.ParameterType.String:
+                    gameEvent.UnregisterListenerString(OnEventRaised);
+                break;
+                case GameEvent.ParameterType.GameObject:
+                    gameEvent.UnregisterListenerGameObject(OnEventRaised); 
+                break;
+            }
+        }
+
+        public void OnEventRaised()
+        { 
+            response.Invoke(); 
+        }
+
+        public void OnEventRaised(string s)
+        {
+            responseString.Invoke(s);
+        }
+
+        public void OnEventRaised(GameObject go)
+        {
+            responseGameObject.Invoke(go);
         }
     }
 }
+
