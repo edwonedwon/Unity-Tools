9d4fbd67b5b66f35d3fb4528963016f133e28425
Edwon <edwonia@gmail.com>
2021-01-24 17:16:24 +0900

    made item pool spawner

---
+++ Packages/Unity Tools/Runtime/ItemPoolSpawner.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Edwon.Tools
+{
+    public class ItemPoolSpawner : MonoBehaviour
+    {
+        public bool debugLog;
+        public enum SpawnOnAwakeOption { Dont, Spawn, SpawnAndHold };
+        public SpawnOnAwakeOption spawnOnAwake = SpawnOnAwakeOption.Dont;
+        public Transform spawnTransform;
+        public Holder holder; // only needed of spawn and hold is called
+        public string setItemToSpawn;
+        public ItemPoolSO itemPool;
+
+        void Awake()
+        {
+            if (name.IsWhiteSpaceOnly())
+                name = gameObject.name;
+
+            if (holder == null)
+                holder = GetComponent<Holder>();
+
+            if (spawnTransform == null)
+                spawnTransform = transform;
+            
+            switch (spawnOnAwake)
+            {
+                case SpawnOnAwakeOption.Spawn:
+                    SpawnSet();
+                    break;
+                case SpawnOnAwakeOption.SpawnAndHold:
+                    SpawnAndHoldSet();
+                    break;
+            }
+        }
+
+        GameObject Spawn(string itemName)
+        {
+            Item spawned = null;
+            spawned = itemPool.SpawnFromPool(itemName, spawnTransform.position, spawnTransform.rotation);
+            if (spawned != null)
+                return spawned.gameObject;
+            else
+                return null;
+        }
+
+        public void SpawnSet()
+        {
+            if (debugLog){ Debug.Log("SpawnSet: " + setItemToSpawn); }
+            Spawn(setItemToSpawn);
+        }
+
+        public void SpawnAndHoldSet()
+        {
+            if (debugLog){ Debug.Log("SpawnAndHoldSet: " + setItemToSpawn); }
+            SpawnAndHold(setItemToSpawn);
+        }
+
+        public void SpawnAndHold(string itemName)
+        {
+            if (debugLog){ Debug.Log("SpawnAndHold " + itemName); }
+            if (holder == null) { Debug.Log("hold is not set on Spawner " + name); return; }
+            GameObject spawned = Spawn(itemName);
+            if (spawned != null)
+                holder.ReleaseAndHold(spawned);
+        }
+        
+        public void SpawnAndHoldAndDestroyHeld(string itemName)
+        {
+            if (debugLog){ Debug.Log("SpawnAndHoldAndDestroyHeld: " + itemName); }
+            if (holder == null) { Debug.Log("holder is not set on Spawner " + name); return; }
+            holder.DestroyHeld();
+            SpawnAndHold(itemName);
+        }
+    }
+}
--- Packages/Unity Tools/Runtime/Spawner.cs
+++ Packages/Unity Tools/Runtime/Spawner.cs
@@ -14,7 +14,6 @@ namespace Edwon.Tools
         public Holder holder; // only needed of spawn and hold is called
         public GameObject prefabToSpawn;
         public ItemStorageSO itemStorage;
-        public ItemPoolSO itemPool;
 
         void Awake()
         {
@@ -43,16 +42,6 @@ namespace Edwon.Tools
             return GameObject.Instantiate(prefab, spawnTransform.position, spawnTransform.rotation);
         }
 
-        GameObject Spawn(string itemName)
-        {
-            Item spawned = null;
-            spawned = itemPool.SpawnFromPool(itemName, spawnTransform.position, spawnTransform.rotation);
-            if (spawned != null)
-                return spawned.gameObject;
-            else
-                return null;
-        }
-
         public void SpawnSet()
         {
             if (debugLog){ Debug.Log("SpawnSet: " + prefabToSpawn.name); }
@@ -72,23 +61,6 @@ namespace Edwon.Tools
 
             GameObject spawned = Spawn(prefab);
             holder.ReleaseAndHold(spawned);
-        }
-
-        public void SpawnAndHold(string itemName)
-        {
-            if (debugLog){ Debug.Log("SpawnAndHold " + itemName); }
-            if (holder == null) { Debug.Log("hold is not set on Spawner " + name); return; }
-            GameObject spawned = Spawn(itemName);
-            if (spawned != null)
-                holder.ReleaseAndHold(spawned);
-        }
-        
-        public void SpawnAndHoldAndDestroyHeld(string itemName)
-        {
-            if (debugLog){ Debug.Log("SpawnAndHoldAndDestroyHeld: " + itemName); }
-            if (holder == null) { Debug.Log("holder is not set on Spawner " + name); return; }
-            holder.DestroyHeld();
-            SpawnAndHold(itemName);
         }
 
         public void SpawnAndHoldAndDestroyHeld(GameObject holdableToSpawn)
