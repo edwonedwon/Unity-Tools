cbbf5e7b836e105071c00217508280203e33cbec
Edwon <edwonia@gmail.com>
2021-02-08 15:02:33 +0900

    added debug movement and follow transform scripts

---
+++ Packages/Unity Tools/Runtime/DebugMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMovement : MonoBehaviour
+{
+    [Header("Position")]
+    public bool position = true;
+    public float timeScalePosition = 1;
+    public float scalePosition = 1;
+    public AnimationCurve xPosCurve;
+    public AnimationCurve yPosCurve;
+    public AnimationCurve zPosCurve;
+    [Header("Rotation")]
+    public bool rotation = true;
+    public float timeScaleRotation = 1;
+    public float scaleRotation = 1;
+    public AnimationCurve xRotCurve;
+    public AnimationCurve yRotCurve;
+    public AnimationCurve zRotCurve;
+
+    Vector3 startPosition;
+    Vector3 startRotation;
+
+    public float randomTimeScaleRange = 1f;
+    Vector3 randomTimeScalePosition;
+    Vector3 randomTimeScaleRotation;
+
+    void Awake()
+    {
+        startPosition = transform.localPosition;
+        startRotation = transform.localRotation.eulerAngles;
+        RandomizeTimeOffsets();
+    }
+
+    [InspectorButton("RandomizeTimeOffsets")]
+    public bool randomizeTimeOffsets;
+    public void RandomizeTimeOffsets()
+    {
+        randomTimeScalePosition = RandomVector3(randomTimeScaleRange);
+        randomTimeScaleRotation = RandomVector3(randomTimeScaleRange);
+    }
+
+    Vector3 RandomVector3(float range)
+    {
+        return new Vector3(
+            Random.Range(-range, range),
+            Random.Range(-range, range),
+            Random.Range(-range, range)
+        );
+    }
+
+    void Update()
+    {
+        if (position)
+        {
+            float x = startPosition.x + (xPosCurve.Evaluate((Time.time * randomTimeScalePosition.x) * timeScalePosition) * scalePosition);
+            float y = startPosition.y + (yPosCurve.Evaluate((Time.time * randomTimeScalePosition.y) * timeScalePosition) * scalePosition);
+            float z = startPosition.z + (zPosCurve.Evaluate((Time.time * randomTimeScalePosition.z) * timeScalePosition) * scalePosition);
+            transform.localPosition = new Vector3(x,y,z);
+        }
+        if (rotation)
+        {
+            float x = startRotation.x + (xRotCurve.Evaluate((Time.time * randomTimeScaleRotation.x) * timeScaleRotation) * scaleRotation);
+            float y = startRotation.y + (yRotCurve.Evaluate((Time.time * randomTimeScaleRotation.y) * timeScaleRotation) * scaleRotation);
+            float z = startRotation.z + (zRotCurve.Evaluate((Time.time * randomTimeScaleRotation.z) * timeScaleRotation) * scaleRotation);
+            transform.localRotation = Quaternion.Euler(x,y,z);
+        }
+    }
+}
---
+++ Packages/Unity Tools/Runtime/FollowTransform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    public Transform follow;
+    public bool followX;
+    public bool followY;
+    public bool followZ;
+    Vector3 startPos;
+
+    void Awake()
+    {
+        startPos = transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float x = followX ? follow.position.x : startPos.x;
+        float y = followY ? follow.position.y : startPos.y;
+        float z = followZ ? follow.position.z : startPos.z;
+        transform.position = new Vector3(x,y,z);
+    }
+}
