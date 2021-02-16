0d5ddf8d2ffeec8e8db8da8c2a2bfc4f790957a5
Edwon <edwonia@gmail.com>
2021-02-16 18:09:46 +0900

    added int and float to debug logger

--- Packages/Unity Tools/Runtime/DebugLogger.cs
+++ Packages/Unity Tools/Runtime/DebugLogger.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class DebugLogger : MonoBehaviour
 {
     public bool debugLog = true;
+    public string extraMessage;
 
     public void DebugLog(string message)
     {
         if (debugLog)
             Debug.Log(message);
+    }
+
+    public void DebugLog(int i)
+    {
+        if (debugLog)
+            Debug.Log(extraMessage + " " + i);
+    }
+
+    public void DebugLog(float f)
+    {
+        if (debugLog)
+            Debug.Log(extraMessage + " " + f);
     }
 }
