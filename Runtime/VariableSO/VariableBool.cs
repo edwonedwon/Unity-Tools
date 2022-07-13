5d9a5b2d4867a9dbf8d6c523e4c7797c2baac435
Edwon <edwonia@gmail.com>
2022-07-13 20:10:06 +1000

    made bool variable

---
+++ Runtime/VariableSO/VariableBool.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System;
+
+namespace Edwon.Tools
+{
+    [CreateAssetMenu(fileName = "Variable Bool", menuName = "Variables/Variable Bool")]
+    public class VariableBool : Variable<bool>
+    {
+        [SerializeField]
+        [ReadOnly]
+        private bool runtimeValue;
+        override public bool RuntimeValue 
+        {
+            get
+            {
+                return runtimeValue;
+            }
+            set
+            {
+                runtimeValue = value;
+            }
+        }
+    }
+}
