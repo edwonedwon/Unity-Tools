a6d53ede3c5d1a691107de5eb1afefa0a976c636
Edwon <edwonia@gmail.com>
2022-08-02 17:15:29 +0800

    game event raiser better inspector

--- Runtime/Game Event/GameEventRaiser.cs
+++ Runtime/Game Event/GameEventRaiser.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace Edwon.Tools
 {
     public class GameEventRaiser : MonoBehaviour
     {
+        [InlineEditor]
         public GameEvent gameEvent;
 
+        [Button]
         public void Raise()
         {
             gameEvent.Raise();
