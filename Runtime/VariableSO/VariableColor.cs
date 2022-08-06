938f74198bf67c035b12a6af34439c9e8fd22b46
Edwon <edwonia@gmail.com>
2022-08-06 13:00:28 +0800

    made VariableColor and VariableString

---
+++ Runtime/VariableSO/VariableColor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Edwon.Tools
+{
+    [CreateAssetMenu(fileName = "Variable Color", menuName = "Variables/Variable Color")]
+    public class VariableColor : Variable<Color>
+    {
+        
+    }
+}
---
+++ Runtime/VariableSO/VariableString.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Edwon.Tools
+{
+    [CreateAssetMenu(fileName = "Variable String", menuName = "Variables/Variable String")]
+    public class VariableString : Variable<string>
+    {
+        
+    }
+}
